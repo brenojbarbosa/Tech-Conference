7b1acc5b3eb1ba0b0e85bc4999fe2b56f2350b5c
Breno Barbosa <brenojbarbosas@gmail.com>
2025-05-09 13:01:27 -0300

    atualizando

--- app/Speakers/style.ts
+++ app/Speakers/style.ts
@@ -3,9 +3,6 @@ import styled from "styled-components"
 export const Container = styled.div`
 background-color: #1f1235;
 color: #fbdd74;
-display: flex;
-justify-content: center;
-align-items: center;
 
 
 
