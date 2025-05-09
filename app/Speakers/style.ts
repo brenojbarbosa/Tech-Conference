13b5b344848e8787c7b0d281631f6911571daf72
Breno Barbosa <brenojbarbosas@gmail.com>
2025-05-09 12:44:45 -0300

    Atualizando projeto

--- app/Speakers/style.ts
+++ app/Speakers/style.ts
@@ -3,6 +3,5 @@ import styled from "styled-components"
 export const Container = styled.div`
 background-color: #1f1235;
 color: #fbdd74;
-height: 100vh;
 
 `
--- app/components/RegisterForm/style.ts
+++ app/components/RegisterForm/style.ts
@@ -3,7 +3,6 @@ import styled from "styled-components"
 export const Container = styled.div`
 background-color: #67568c;
 color: #fbdd74;
-height: 80vh;
 margin-top: 30px;
 
 `
