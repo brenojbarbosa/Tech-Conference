3d341cad02a33e4ad3aa67a8a4c97b432b217b63
Breno Barbosa <brenojbarbosas@gmail.com>
2025-05-09 12:49:29 -0300

    atualizando

--- app/Speakers/style.ts
+++ app/Speakers/style.ts
@@ -3,5 +3,9 @@ import styled from "styled-components"
 export const Container = styled.div`
 background-color: #1f1235;
 color: #fbdd74;
+display: flex;
+justify-content: center;
+align-items: center;
+
 
 `
--- app/components/RegisterForm/style.ts
+++ app/components/RegisterForm/style.ts
@@ -4,6 +4,9 @@ export const Container = styled.div`
 background-color: #67568c;
 color: #fbdd74;
 margin-top: 30px;
+display: flex;
+justify-content: center;
+align-items: center;
 
 `
 
